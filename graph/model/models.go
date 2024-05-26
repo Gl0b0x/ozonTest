24595de200b58cf990407802edcdb438cb40d013
Ivan Kuznetsov <nepisatmne80@gmail.com>
2024-05-26 12:19:29 +0300

    init

---
+++ graph/model/models.go
@@ -0,0 +1,35 @@
+package model
+
+type Comment struct {
+	ID        string  `json:"id"`
+	PostID    string  `json:"postId"`
+	ParentID  *string `json:"parentId,omitempty"`
+	Content   string  `json:"content"`
+	CreatedAt string  `json:"createdAt"`
+	Author    *User   `json:"author"`
+}
+
+type Mutation struct {
+}
+
+type Post struct {
+	ID              string     `json:"id"`
+	Title           string     `json:"title"`
+	Content         string     `json:"content"`
+	Comments        []*Comment `json:"comments,omitempty"`
+	CommentsEnabled bool       `json:"commentsEnabled"`
+	Author          *User      `json:"author"`
+}
+
+type Query struct {
+}
+
+type Subscription struct {
+}
+
+type User struct {
+	ID       string  `json:"id"`
+	Username string  `json:"username"`
+	Email    string  `json:"email"`
+	Posts    []*Post `json:"posts,omitempty"`
+}
